df00354b7efe649c0781f61ad5392e7e6834bb29
Jeandpc123 <91077401+Jeandpc123@users.noreply.github.com>
2021-09-21 17:01:54 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -11,8 +11,6 @@ const help = (prefix) => {
 ◉ *COMANDOS PARA ACTIVAR*
 *loli 1
 *leveling 1
-*play (coloca el nombre de la cancion)
-*you (link del video)
 *welcome 1 (bienvenida al grupo)
 *welcome 0 (desactivar la bivenida)
 ◉ *CREAR STICKERS*
@@ -23,9 +21,12 @@ const help = (prefix) => {
 ◉ *CONVERTIDORES*
   ╠ ○ ${prefix}toimg
   ╠ De sticker a JPG
+  ◉ *DESCARGAR VIDEO*
+  ╠ *you (link del video youtube)
 ◉ *AUDIO*
   ╠ ○ ${prefix}idioma
   ╚ ○ ${prefix}tts es (mas texto)
+  *play (coloca el nombre de la cancion)
   ╠ Link de Whatsapp
   ╠ ○ ${prefix}qrcode
   ╚ Coloca un texto
