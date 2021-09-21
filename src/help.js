250c52b905edcf8aa1dec91ed6ba4a73cf79d8ba
Jeandpc123 <91077401+Jeandpc123@users.noreply.github.com>
2021-09-21 16:54:57 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,13 +1,16 @@
 const help = (prefix) => {
 	return `
 ⌜ *ѕнαηвσт ву BOTCITO* ⌟  
-*leveling 1
-*levl
+*level
 *otak
 *shantera
 *rankgay
 *gay
 *cuties
+*ban (este comando te banea del bot)
+◉ *COMANDOS PARA ACTIVAR*
+*loli 1
+*leveling 1
 *play (coloca el nombre de la cancion)
 *you (link del video)
 *welcome 1 (bienvenida al grupo)
@@ -20,21 +23,14 @@ const help = (prefix) => {
 ◉ *CONVERTIDORES*
   ╠ ○ ${prefix}toimg
   ╠ De sticker a JPG
-  ╠ ○ ${prefix}tomp3
-  ╚ De video a MP3
 ◉ *AUDIO*
   ╠ ○ ${prefix}idioma
   ╚ ○ ${prefix}tts es (mas texto)
-◉ *OTROS*
   ╠ Link de Whatsapp
   ╠ ○ ${prefix}qrcode
   ╚ Coloca un texto
-◉ *GRUPOS*
-  ╠ ○ ${prefix}kick
-  ╠ Eliminar a un miembro 
-  ╠ ○ ${prefix}linkgc
-_*by shanduy*_
-⌜ *ごきげんよう :)* ⌟ 
+*by shanduy*
+*by TERNA-DPC*
 `
 }
 
